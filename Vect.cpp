d487242656bb1a67bbfbf0992eacff2215dea930
Anton Akzhigitov <akzhigitov.anton@yandex.ru>
2011-12-06 01:08:31 +0400

    modified Vect

---
+++ Vect.cpp
@@ -0,0 +1,48 @@
+#include <Vect.h>
+
+TVect::TVect(int Count)
+{
+    data=new double[Count];
+    for(int i=0;i<Count;i++)
+        data[i]=0;
+    Length=Count;
+}
+
+TVect::TVect(int Count, double *n)
+{
+    data=new double [Count];
+    for(int i=0;i<Count;i++)
+        data[i]=n[i];
+    Length=Count;
+}
+
+void TVect::setLength(int newcount)
+{
+    delete [] data;
+    data=new double[newcount];
+    Length=newcount;
+}
+
+int TVect::getLength()
+{
+    return Length;
+}
+
+void TVect::setElement(int number,double value)
+{
+   this->data[number]=value;
+}
+
+double TVect::getElement(int number)
+{
+  return data[number];
+}
+
+double TVect::getMagnitude()
+{
+    double s;
+    s=0;
+    for(int i=0;i<=Length-1;i++)
+    s=s+pow(data[i],2);
+    return sqrt(s);
+}
---
+++ Vect.h
@@ -0,0 +1,68 @@
+#ifndef VECT_H
+#define VECT_H
+
+#endif // VECT_H
+#include <math.h>
+class TVect
+{
+private:
+    double *data;
+    int Length;
+public:
+    void setLength(int newcount);
+    int getLength();
+    TVect(int Count);
+    TVect(int Count, double *n);
+    TVect* operator + (const TVect &Vect)const
+    {
+        TVect *resultVect;
+        resultVect=new TVect(this->Length);
+        for(int i=0;i<=this->Length-1;i++)
+        resultVect->data[i]=this->data[i]+Vect.data[i];
+        return resultVect;
+    }
+
+    TVect* operator - (const TVect &Vect)const
+    {
+        TVect *resultVect;
+        resultVect=new TVect(this->Length);
+        for(int i=0;i<=this->Length-1;i++)
+        resultVect->data[i]=this->data[i]-Vect.data[i];
+        return resultVect;
+    }
+
+    TVect* operator % (const TVect &cross)const
+    {
+        if(this->Length==3 && cross.Length==this->Length)
+        {
+            TVect *tmpVect;
+            tmpVect=new TVect(3);
+            tmpVect->data[0]=this->data[1]*cross.data[2]-this->data[2]*cross.data[1];
+            tmpVect->data[1]=this->data[2]*cross.data[0]-this->data[0]*cross.data[2];
+            tmpVect->data[2]=this->data[0]*cross.data[1]-this->data[1]*cross.data[0];
+            return tmpVect;
+        }
+    }
+
+    TVect* operator * (const double cross) const
+    {
+        TVect *tmpVect;
+        tmpVect=new TVect(this->Length);
+        for (int i=0;i<=Length-1;i++)
+        tmpVect->data[i]=this->data[i]*cross;
+        return tmpVect;
+    }
+
+    double operator * (const TVect &cross) const
+    {
+        double tmp;
+        tmp=0;
+        for (int i=0;i<Length;i++)
+        tmp+=this->data[i]*cross.data[i];
+        return tmp;
+    }
+
+    double getElement(int number);
+    double getMagnitude();
+    void setElement(int number,double value);
+};
